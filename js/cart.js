710b0dcbe617a317fb80ca2aaafc2cb9f8af0439
Mahfuzur  Rahman <93132951+ripon301252@users.noreply.github.com>
2025-09-07 23:40:44 +0600

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,18 @@
  document.getElementById('card-container').addEventListener('click', (e) => {
-    // console.log(e)
+
+    const totalPrice = document.getElementById('total-price').innerText;
+   
+    let treeName = '';
+    let price = '';
+
     if(e.target.className.includes('cart-title')){
         const cartBtn = e.target
-        const treeName = cartBtn.parentNode.children[1].innerText;
-        const price = cartBtn.parentNode.children[3].children[1].children[1].innerText;
-        console.log(price)
-       
+        treeName = cartBtn.parentNode.children[1].innerText;
+        price = cartBtn.parentNode.children[3].children[1].children[1].innerText;
+        // console.log(price)
+        const priceTotal = Number(price) + Number(totalPrice);
+
+       document.getElementById('total-price').innerText = priceTotal;
         alert(`${treeName} has been added to the cart.`);
     }
 
@@ -15,17 +22,33 @@
     cartHistory.innerHTML = `
         <div class=" bg-[#f0fdf4] rounded-md flex justify-between items-center p-3 mt-3">
             <div>
-                <h1 class="font-bold">Tree name</h1>
-                <p class="text-[#889396]"><span>৳</span><span id="price-title"> 1200</span> x 1</p>
+                <h1 class="font-bold">${treeName}</h1>
+                <p class="text-[#889396] "><span>৳</span><span id="price-title"> ${price}</span> x 1</p>
             </div>
             <div class="">
-                <p>❌</p>
+                <p id = 'delete' class = 'cursor-pointer text-2xl text-[#889396]'>×</p>
             </div>
         </div>
-               
-        
     `;
     cartContainer.appendChild(cartHistory)
-
-
  })
+
+
+ // Delete feature
+document.getElementById('cart-container').addEventListener('click', (e) => {
+
+    if (e.target.id === 'delete') {
+        
+        const perCart = e.target.parentNode.parentNode.parentNode;
+        
+        
+        const price = Number(perCart.querySelector('#price-title').innerText);
+
+        const totalPrices = document.getElementById('total-price');
+        const currentTotal = Number(totalPrices.innerText);
+        totalPrices.innerText = currentTotal - price;
+
+       
+        perCart.remove();
+    }
+});
