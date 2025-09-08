e8ccb4e13f7a16b6d8d2e8e8745210c8eec72605
Mahfuzur  Rahman <93132951+ripon301252@users.noreply.github.com>
2025-09-08 08:55:09 +0600

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -8,16 +8,6 @@ const loadTreeDetails = (modalCardId) => {
 }
 
 
-
-// {
-//     "id": 30,
-//     "image": "https://i.ibb.co.com/0jLycYdv/Water-Hyacinth-min.jpg",
-//     "name": "Water Hyacinth",
-//     "description": "A floating plant with violet flowers that provide shade to aquatic creatures. Known for rapid growth in ponds.",
-//     "category": "Aquatic Plant",
-//     "price": 250
-// }
-
 const displayTreeDetails = (tree) => {
     console.log(tree)
     const treeDetailsContainer = document.getElementById('tree-details-container');
@@ -25,14 +15,11 @@ const displayTreeDetails = (tree) => {
       
         <div class = 'space-y-5 '>
             <h3 class="text-lg font-bold">${tree.name}</h3>
-            <div><img class = ' md:w-[462px] w-[262px] md:h-[230px] h-[130px] rounded-md' src="${tree.image}" alt=""></div>
+            <div><img class = ' md:w-[465px] w-[300px] md:h-[238px] h-[150px] rounded-md' src="${tree.image}" alt=""></div>
             <p class="text-sm"><span class = 'font-bold text-base'>Category : </span>${tree.category}</p>
             <p class="text-base font-bold"><span>৳</span> <span>${tree.price}</span></p>
             <p class="text-sm"><span class = 'font-bold text-base'>Description :</span> ${tree.description}</p>
         </div>
-
-    
-    
     `
     document.getElementById('tree_modal').showModal()
 }
